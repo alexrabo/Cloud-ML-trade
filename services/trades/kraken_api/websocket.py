f971b9626e47f6cd1e982338bf60c224432cefac
Paulescu <plabartabajo@gmail.com>
2024-12-04 12:17:31 +0100

    Rename pair to _ in websocket.py for demo purposes

--- services/trades/kraken_api/websocket.py
+++ services/trades/kraken_api/websocket.py
@@ -76,7 +76,7 @@ class KrakenWebsocketAPI:
             )
         )
 
-        for _pair in self.pairs:
+        for _ in self.pairs:
             _ = self._ws_client.recv()
             _ = self._ws_client.recv()
 
